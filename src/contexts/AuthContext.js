406179be20a2a66e9bcd1c7d9ad3fafff797ea26
Zlatina Moga <zlatina.delcheva@gmail.com>
2021-12-08 17:30:01 +0200

    updated authcontext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
+
+const initialState = {
+    _id: '',
+    accessToken: '',
+    email: '',
+    name: ''
+}
+
+export const AuthProvider = ({
+    children
+}) => {
+    const [user, setUser] = useState(initialState)
+    return (
+        <AuthContext.Provider value={{user}}> 
+            {children}
+        </AuthContext.Provider>
+    )
+}
