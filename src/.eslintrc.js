1486af5ae221226a252fd6a89fbbb256710e3662
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-02-10 16:53:58 +0200

    Updates

--- src/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: 'react-app',
-    rules: {
-        'eqeqeq': 'off',
-        'jsx-a11y/anchor-is-valid': 'off',
-        'no-unused-vars': 'off',
-        'jsx-a11y/alt-text': 'off'
-    }
-}
