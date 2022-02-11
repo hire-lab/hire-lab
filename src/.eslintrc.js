fc4f8997cee6caf16be27ede4e6c43cf450ed916
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-02-11 12:12:22 +0200

    updated list of warnings

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     rules: {
         'react/jsx-no-target-blank': 'off',
         'jsx-a11y/anchor-has-content': 'off',
-        'jsx-a11y/anchor-is-valid': 'off'
+        'jsx-a11y/anchor-is-valid': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        'eqeqeq': 'off',
+        'no-throw-literal': 'off'
     }
 }
