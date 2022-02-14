007d84df87dae1e80221e2fb681d8423701e9424
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-02-14 18:09:15 +0200

    added aply btn for users

--- src/components/JobsCatalog/JobDetails/JobDetails.js
+++ src/components/JobsCatalog/JobDetails/JobDetails.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useHistory} from "react-router-dom";
-import { useAuthContext } from "../../../contexts/AuthContext";
+import { useCompanyAuthContext } from "../../../contexts/AuthCompanyContext";
+import {useAuthContext} from '../../../contexts/AuthContext';
 import * as jobService from '../../../services/JobService';
+import * as candidateService from '../../../services/CandidateService';
 
 import './JobDetails.css';
 
 export default function JobDetails() {
     const history = useHistory();
     let {jobId} = useParams()
+    const {company} = useCompanyAuthContext();
     const {user} = useAuthContext();
     const [job, setJob] = useState({});
-
+    let actions = '';
 
     useEffect(() => {
         jobService.getOne(jobId)
@@ -23,13 +26,32 @@ export default function JobDetails() {
     const deleteHandler = (e) => {
         e.preventDefault();
 
-        jobService.del(jobId, user.accessToken)
+        jobService.del(jobId, company.accessToken)
             .then(() => {
                 history.push('/jobs')
             })
     }
 
-    const authButtons = (
+    const onApplyBtn = (e) => {
+        e.preventDefault();
+
+        let name = user.name;
+        let email = user.email;
+
+        let candidate = {
+            name,
+            email,
+            jobId
+        }
+
+        console.log(candidate)
+
+        candidateService.create(candidate).then(() =>{
+                    history.push('/jobs')
+                })
+    }
+
+    const companyButtons = (
         <div className="actions">
             <Link className="jobDetailsButton" to={`/jobs/edit/${job._id}`}>Edit</Link>
             <Link className="jobDetailsButton" to='#' onClick={deleteHandler}>Delete</Link>
@@ -39,21 +61,37 @@ export default function JobDetails() {
         </div>
     )
 
+    const guestButton = (<Link className="loginBtn guestBtn" to="/login">Login to Apply</Link>)
+
+    const userButton = (<Link className="loginBtn guestBtn" onClick={onApplyBtn} to="#">Apply</Link>)
+
+    if (company.email != ''){
+        actions = companyButtons
+    } else if (user.email != ''){
+        actions = userButton
+    } else {
+        actions = guestButton
+    }
+
     return (
         <section className="jobDetails">
         <div className="jobDetailsInformation">
             <h3>Title:</h3>
             <h1>{job.title}</h1>
             <div className="jobDescription">
+                <h3>Company:</h3>
+                <p>{job.companyName}</p>
+            </div>
+            <div className="jobDescription">
+                <h3>Location:</h3>
+                <p>{job.location}</p>
+            </div>
+            <div className="jobDescription">
                 <h3>Description:</h3>
                 <p>{job.description}</p>
             </div>
 
-            {user.email
-                ? authButtons
-                : <Link className="loginBtn guestBtn" to="/login">Login to Apply</Link>
-            }
-
+            {actions}
 
         </div>
        
