70aedf846f61645debb114a3a5f1e51601612e25
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-02-22 17:43:32 +0200

    adding date and time sort to interviews

--- src/components/InterviewsCatalog/InterviewsByJobId/InterviewsByJobId.js
+++ src/components/InterviewsCatalog/InterviewsByJobId/InterviewsByJobId.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Calendar from "react-calendar";
 import * as interviewService from '../../../services/InterviewService'
 
 export default function Interview(){
     let {jobId} = useParams();
-    const [interviews, setInterviews] = useState([]);
+    let [interviews, setInterviews] = useState([]);
+    let [date, setDate] = useState(new Date());
 
     useEffect(() => {
         interviewService.getByJobId(jobId)
@@ -13,18 +15,27 @@ export default function Interview(){
             })
     }, [jobId])
 
+    const onDateClick = (e) => {
+        //adjust date formats and compare
+        console.log(e)
+    }
+
     return (
         <section className="interviewsList">
             <article className="loginPageTitle">
                 <h1>Upcoming Interviews:</h1>
             </article>
-    <ul>
-        {interviews.length > 0 
-        ? interviews.map(i => 
-            <li key={i._id}>{i.candidateId.name} - {i.jobId.title}</li>)
-        :   <h4>There are no interviews yet.</h4>
-        }
-    </ul>
+
+            <div className="calendarWrapper">
+                    <Calendar onChange={onDateClick} value={date}/>
+            </div>
+        <ul>
+            {interviews.length > 0 
+                ? interviews.map(i => 
+                <li key={i._id}>{i.date} {i.time} - {i.candidateId.name} - {i.jobId.title}</li>)
+            :       <h4>There are no interviews yet.</h4>
+            }
+        </ul>
     </section>
     )
 }
