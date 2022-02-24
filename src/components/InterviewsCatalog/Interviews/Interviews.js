a92051d1ecf4f6f951f2bb7b428627885ce15817
Zlatina Moga <zlatina.delcheva@gmail.com>
2022-02-24 08:43:47 +0200

    filtering interviews by date

--- src/components/InterviewsCatalog/Interviews/Interviews.js
+++ src/components/InterviewsCatalog/Interviews/Interviews.js
@@ -16,7 +16,8 @@ export default function Interviews(){
             .then(result => {
                 setInterviews(result)
             })
-    }, [companyId])
+    }, [companyId, date])
+
 
     return (
         <section className="interviewsList">
@@ -40,25 +41,4 @@ export default function Interviews(){
     )
 }
 
-
-/*
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
-import moment from "moment";
-export default function Interview() {
-    const [dateState, setDateState] = useState(new Date())
-    const changeDate = (e) => {
-        setDateState(e)
-    }
-    return (
-        <section className="interview-calendar">
-            <h1 className="interview-calendar-title">Interview List</h1>
-            <Calendar 
-                 value={dateState}
-                 onChange={changeDate}
-            />
-            <p className="interview-calendar-info">Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>
-        </section>
-    )
-}*/
 
